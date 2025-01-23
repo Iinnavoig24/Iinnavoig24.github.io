0973d08a1ab385922d378f0a2c0204c190fbb819
Iinnavoig24 <desantisgiovanni016@gmail.com>
2025-01-23 10:59:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+0; // this is a comment, which means that javascript will not try to execute this part
+134151855;
+12341.1515;
+Infinity;
+
+var kitty; // declared a variable for store space in memeory;
+
+kitty = 5; // definition with a data type of number with the value 5
+
+var Kitty = 24; // semi are end of statment, this line has a decloration and definition
+
+console.log(Kitty);
+
+kitty = Kitty; // assignment operator right to left
+
+console.log(kitty);
+
+var doggy; // declared a variable but not defined a value
+console.log(doggy); // undefined
+
+// numbers, undefined, strings 
+
+var prose = 'ubvefbviubducbidasbub  '; // string delimiter
+var moreprose = "i said 'something' ecugiv";
+
+// booleans are either true or false 
+
+var x = true;
+x = false;
+
+// objects
+var ball = {}; // empty object
+ball = null; // null is a datatype in javascript 
+
+// operators
+5 + 5;
+kitty = 5 + 5;
+var kitty; // becomes undefined because we are now refferring to a new place in memeory
+
+kitty = 5 + "5";
+console.log(kitty);
+
+console.log(kitty + "ubdvbdub");
